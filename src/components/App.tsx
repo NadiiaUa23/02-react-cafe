53762b056bc1438f5dafdd534341a6d31f98121c
NadiiaUa23 <hasiminka92@gmail.com>
2025-06-26 14:46:10 +0200

    deler task

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+// import { useState } from "react";
+import css from "./App.module.css";
+
+function App() {
+  return (
+    <div className={css.app}>
+      <h1>holla</h1>
+    </div>
+  );
+}
+
+export default App;
